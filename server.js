a4075e364ea87892ebd77b17bb603a23eb0e7401
Rushin Shah <shahrushin32@gmail.com>
2024-08-02 14:32:56 +0530

    Initial Commit.

---
+++ server.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+dotenv.config();
+app.use(cors());
+
+async function main() {
+  try {
+    const MONGOURI = process.env.MONGO_URI;
+    await mongoose.connect(MONGOURI);
+    console.log("Connected to DB.");
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+main();
