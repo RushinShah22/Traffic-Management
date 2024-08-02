d65a85dee8dd4bf2247ef88fd1cc66a5945ff102
Rushin Shah <shahrushin32@gmail.com>
2024-08-02 14:41:22 +0530

    Add Routes.

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const locationRoute = require("./routes/locations");
+const roadsRouter = require("./routes/roads");
+const shortestPathRouter = require("./routes/shortestPath");
+const trafficUpdateRouter = require("./routes/trafficUpdates");
 
 const app = express();
+
+app.use("/locations", locationRoute);
+app.use("/roads", roadsRouter);
+app.use("/shortest-path", shortestPathRouter);
+app.use("/traffic-update", trafficUpdateRouter);
 
 module.exports = app;
